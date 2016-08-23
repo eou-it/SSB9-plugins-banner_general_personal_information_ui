7541d09b92c645513936137815484106b10de441
Daren_Dunn <daren.dunn@ellucian.com>
2016-08-23 10:02:14 -0400

    BXEGS-259 implement XE Component library tabs

--- web-app/js/xe-components/xe-ui-components.js
+++ web-app/js/xe-components/xe-ui-components.js
@@ -3087,10 +3087,14 @@ angular.module('dataTableModule', ['utils'])
                         script.src = activeTab.jsLazyLoad;
                         document.head.appendChild(script);
                     };
-                    $transclude(scope, function (clone, scope) {
+                    // Use closure to set tab scope property so transcluded content can use its default scope
+                    var setHasTranscludedContentTrue = function(){
+                        scope.hasTranscludedContent = true;
+                    };
+                    $transclude(function (clone, scope) {
                         var elementTo;
                         if (clone.text().trim().length) {
-                            scope.hasTranscludedContent = true;
+                            setHasTranscludedContentTrue();
                             elementTo = angular.element(ele[0].querySelector('[content]'));
                             elementTo.append(clone);
                         }
