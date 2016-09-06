53bb7f6875e93319b8ce8e9b4b907039d53fad6e
Daren_Dunn <daren.dunn@ellucian.com>
2016-09-06 16:16:52 -0400

    BXEGS-278 add error styling for edit email modal

--- web-app/personalInformationApp/piEmail/piEditEmail-controller.js
+++ web-app/personalInformationApp/piEmail/piEditEmail-controller.js
@@ -5,6 +5,9 @@ personalInformationAppControllers.controller('piEditEmailController',['$scope', 
         // CONTROLLER VARIABLES
         // --------------------
         $scope.isCreateNew = true;
+        $scope.emailTypeErrMsg = '';
+        $scope.emailAddressErrMsg = '';
+        $scope.emailErrMsg = '';
 
 
         // CONTROLLER FUNCTIONS
@@ -14,20 +17,38 @@ personalInformationAppControllers.controller('piEditEmailController',['$scope', 
             notificationCenterService.clearNotifications();
         };
 
-        $scope.removeAddressFieldErrors = function(){
-            console.log('boo!');
+        $scope.removeEmailFieldErrors = function() {
+            if(!!$scope.emailTypeErrMsg) {
+                $scope.emailTypeErrMsg = piEmailService.getErrorEmailType($scope.email);
+            }
+            if(!!$scope.emailAddressErrMsg) {
+                $scope.emailAddressErrMsg = piEmailService.getErrorEmailAddress($scope.email);
+            }
+        };
+
+        var isValidEmail = function () {
+            $scope.emailTypeErrMsg = piEmailService.getErrorEmailType($scope.email);
+            $scope.emailAddressErrMsg = piEmailService.getErrorEmailAddress($scope.email);
+
+            return !($scope.emailTypeErrMsg || $scope.emailAddressErrMsg);
         };
 
         $scope.saveEmail = function() {
+            if (isValidEmail()) {
                 var handleResponse = function (response) {
                     if (response.failure) {
                         notificationCenterService.displayNotification(response.message, "error");
+                        $scope.emailErrMsg = response.message;
                     }
                     else {
+                        notificationCenterService.clearNotifications();
+
                         var notifications = [];
-                        notifications.push({message: 'personInfo.save.success.message',
+                        notifications.push({
+                                message: 'personInfo.save.success.message',
                                 messageType: $scope.notificationSuccessType,
-                                flashType: $scope.flashNotification}
+                                flashType: $scope.flashNotification
+                            }
                         );
 
                         $state.go('personalInformationMain',
@@ -37,22 +58,26 @@ personalInformationAppControllers.controller('piEditEmailController',['$scope', 
                     }
                 };
 
-                if($scope.isCreateNew) {
+                if ($scope.isCreateNew) {
                     piCrudService.create('Email', $scope.email).$promise.then(handleResponse);
                 }
                 else {
                     piCrudService.update('Email', $scope.email).$promise.then(handleResponse);
                 }
+            }
+            else {
+                piEmailService.displayMessages();
+            }
         };
 
         this.init = function() {
 
             if (editEmailProperties.currentEmail) {
-                // Set up for "update address"
+                // Set up for "update email"
                 $scope.isCreateNew = false;
                 $scope.email = angular.copy(editEmailProperties.currentEmail);
             } else {
-                // Create "new address" object
+                // Create "new email" object
                 $scope.email = {
                     emailType: {},
                     emailAddress: '',
