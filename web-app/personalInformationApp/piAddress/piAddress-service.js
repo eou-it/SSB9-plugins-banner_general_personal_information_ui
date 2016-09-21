9377e84a441cc643a5a64fc45b391b3fd75241de
Daren_Dunn <daren.dunn@ellucian.com>
2016-09-21 17:23:46 -0400

    BXEGS-255 add space for generic errors to edit address layout, add date validation to UI

--- web-app/personalInformationApp/piAddress/piAddress-service.js
+++ web-app/personalInformationApp/piAddress/piAddress-service.js
@@ -1,5 +1,5 @@
-personalInformationApp.service('piAddressService', ['notificationCenterService',
-    function (notificationCenterService) {
+personalInformationApp.service('piAddressService', ['notificationCenterService', '$filter',
+    function (notificationCenterService, $filter) {
 
         var messages = [];
 
@@ -24,6 +24,48 @@ personalInformationApp.service('piAddressService', ['notificationCenterService',
             }
             else {
                 notificationCenterService.removeNotification(msg);
+            }
+        };
+
+        this.getErrorDateRange = function (address, addressList) {
+            var msg = 'personInfo.address.error.dateRange';
+            if (address.fromDate) {
+                var MAX_DATE = 8640000000000000;
+                var fromDate = new Date(Date.parse(address.fromDate));
+                var toDate = address.toDate ? new Date(Date.parse(address.toDate)) : new Date(MAX_DATE) ;
+                var flatList = _.flatten(addressList);
+
+                var overlappedAddress = _.find(flatList,
+                    function(listItem){
+                        var isRangeError = false;
+                        if(address.id !== listItem.id) {
+                            var listFromDate = new Date(Date.parse(listItem.fromDate));
+                            var listToDate = listItem.toDate ? new Date(Date.parse(listItem.toDate)) : new Date(MAX_DATE);
+
+                            if (fromDate < listToDate) {
+                                isRangeError = toDate >= listFromDate;
+                            }
+                            else {
+                                isRangeError = fromDate === listToDate;
+                            }
+                        }
+                        return isRangeError;
+                    }
+                );
+
+                if(overlappedAddress){
+                    var data = [];
+                    data[0] = overlappedAddress.fromDate;
+                    data[1] = overlappedAddress.toDate ?
+                        overlappedAddress.toDate : '('+ $filter('i18n')('personInfo.no.end.date') +')';
+                    msg = $filter('i18n')(msg, data);
+                    messages.push({msg: msg, type: 'error'});
+
+                    return msg;
+                }
+                else {
+                    notificationCenterService.removeNotification(msg);
+                }
             }
         };
 
