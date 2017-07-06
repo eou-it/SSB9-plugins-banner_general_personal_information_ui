401b4ebeaba41b3a65524fd90dd1f7264ce213e1
Daren_Dunn <daren.dunn@ellucian.com>
2017-07-06 16:04:05 -0400

    BXEGS-938 refactor PersonalInformationConfigService for sub-app enabling/disabling

---
+++ grails-app/services/net/hedtech/banner/general/BasePersonConfigService.groovy
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ Copyright 2017 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+package net.hedtech.banner.general
+
+import net.hedtech.banner.general.overall.IntegrationConfiguration
+import net.hedtech.banner.general.person.PersonUtility
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional(readOnly = false, propagation = Propagation.REQUIRED )
+abstract class BasePersonConfigService {
+    
+    protected abstract String getCacheName();
+
+    protected abstract String getProccessCode();
+
+    protected abstract List getExcludedProperties();
+
+    def getParamFromSession(param, defaultVal) {
+        def personalInfoConfig = getPersonConfigFromSession(getCacheName(), getProccessCode(), getExcludedProperties())
+
+        def paramVal = personalInfoConfig[param]
+
+        if (!paramVal) {
+            log.error("No value found for integration configuration setting \"" + param + "\". " +
+                    "This should be configured in GORICCR. Using default value of \"" + defaultVal + "\".")
+
+            paramVal = defaultVal
+        }
+
+        paramVal
+    }
+
+    private static getPersonConfigFromSession(cacheName, processCode, excludedProperties) {
+        def personConfigInSession = PersonUtility.getPersonConfigFromSession()
+
+        if (personConfigInSession) {
+            if (!personConfigInSession.containsKey(cacheName)) {
+                createConfig(personConfigInSession, cacheName, processCode, excludedProperties)
+            }
+        } else {
+            createConfig(personConfigInSession, cacheName, processCode, excludedProperties)
+            PersonUtility.setPersonConfigInSession(personConfigInSession)
+        }
+
+        personConfigInSession[cacheName]
+    }
+
+    private static createConfig(personConfigInSession, cacheName, processCode, excludedProperties) {
+        def configFromGoriccr = IntegrationConfiguration.fetchAllByProcessCode(processCode)
+        def config = [:]
+
+        configFromGoriccr.each {it ->
+            if (!excludedProperties.contains(it.settingName)) {
+                config[it.settingName] = it.value
+            }
+        }
+
+        personConfigInSession[cacheName] = config
+
+        personConfigInSession
+    }
+}
