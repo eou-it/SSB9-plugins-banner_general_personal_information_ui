a98d1c7aa9faaf11b09b1b9322d7e8e73986a05e
Daren_Dunn <daren.dunn@ellucian.com>
2018-11-30 11:32:46 -0500

    BXEGS-1477 refactor to remove dead code

--- grails-app/controllers/net/hedtech/banner/general/PersonalInformationDetailsController.groovy
+++ grails-app/controllers/net/hedtech/banner/general/PersonalInformationDetailsController.groovy
@@ -213,38 +213,6 @@ class PersonalInformationDetailsController {
     def updateAddress() {
         try {
             checkActionPermittedPerConfiguration([
-                  name: PersonalInformationConfigService.ADDR_MODE,
-                  minRequiredMode: PersonalInformationConfigService.SECTION_UPDATEABLE
-            ])
-        } catch (ApplicationException e) {
-            render PersonalInformationControllerUtility.returnFailureMessage(e) as JSON
-            return
-        }
-
-        def updatedAddress = request?.JSON ?: params
-        updatedAddress.pidm = PersonalInformationControllerUtility.getPrincipalPidm()
-
-        fixJSONObjectForCast(updatedAddress)
-
-        try {
-            personAddressService.checkAddressFieldsValid(updatedAddress)
-
-            updatedAddress = personalInformationCompositeService.getPersonValidationObjects(updatedAddress, getRoles())
-
-            convertAddressDates(updatedAddress)
-            personAddressCompositeService.checkDatesForUpdate(updatedAddress)
-
-            personAddressService.update(updatedAddress)
-            render([failure: false] as JSON)
-        }
-        catch (ApplicationException e) {
-            render PersonalInformationControllerUtility.returnFailureMessage(e) as JSON
-        }
-    }
-
-    def updateAddressWithoutOverwrite() {
-        try {
-            checkActionPermittedPerConfiguration([
                     name: PersonalInformationConfigService.ADDR_MODE,
                     minRequiredMode: PersonalInformationConfigService.SECTION_UPDATEABLE
             ])
