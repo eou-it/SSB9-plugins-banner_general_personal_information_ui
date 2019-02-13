1bd47a1f331e01c4ba395a6f58335254f8256e69
Daren_Dunn <daren.dunn@ellucian.com>
2019-02-13 14:37:47 -0500

    BXEGS-1497 do not allow preferred email changes if existing preferred is not visible

--- test/integration/net/hedtech/banner/general/PersonalInformationDetailsControllerTests.groovy
+++ test/integration/net/hedtech/banner/general/PersonalInformationDetailsControllerTests.groovy
@@ -490,6 +490,7 @@ class PersonalInformationDetailsControllerTests extends BaseIntegrationTestCase 
         assertNotNull data
         assertEquals 2, data.emails.size()
         assertEquals 'ansbates@telstra.com', data.emails[0].emailAddress
+        assertTrue data.isPreferredEmailVisible
     }
 
     @Test
@@ -510,6 +511,21 @@ class PersonalInformationDetailsControllerTests extends BaseIntegrationTestCase 
         def data = JSON.parse( dataForNullCheck )
         assertNotNull data
         assertTrue data.failure
+    }
+
+    @Test
+    void testGetEmailsPreferredInvisible() {
+        loginSSB 'GDP000003', '111111'
+
+        controller.request.contentType = "text/json"
+        controller.getEmails()
+
+        def dataForNullCheck = controller.response.contentAsString
+        def data = JSON.parse( dataForNullCheck )
+        assertNotNull data
+        assertEquals 1, data.emails.size()
+        assertEquals 'batescry@theweb.net', data.emails[0].emailAddress
+        assertFalse data.isPreferredEmailVisible
     }
 
     @Test
