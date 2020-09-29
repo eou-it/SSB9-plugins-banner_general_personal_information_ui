6ad98aadef7651a4915efea9bad4c3728543196e
Supritha Kulkarni <supritha.shrikantkulkarni@ellucian.com>
2020-09-29 16:22:44 +0530

    Removing the redirection to login denied page.

--- grails-app/assets/javascripts/personalInformationApp/app.js
+++ grails-app/assets/javascripts/personalInformationApp/app.js
@@ -184,9 +184,6 @@ personalInformationApp.config(['$httpProvider',
                 },
                 'responseError': function(rejection) {
                     checker(rejection.config,"response");
-                    if (rejection.status === 403) {
-                        window.location.href = '/login/denied';
-                    }
                     return $q.reject(rejection);
                 }
             };
